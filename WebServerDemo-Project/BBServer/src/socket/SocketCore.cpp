23a97e937e05719132ae80455a21e5859a5fbdb0
烛九 <zhujiu@bytedance.com>
2021-03-30 10:28:29 +0800

    update BBServer && C++ cookies knowledge update

--- WebServerDemo-Project/BBServer/src/socket/SocketCore.cpp
+++ WebServerDemo-Project/BBServer/src/socket/SocketCore.cpp
@@ -1,17 +1,61 @@
 #include "SocketCore.h"
 
 namespace NetWork {
+    enum Status {
+        Success = 1,
+        Failure = 2,
+    };
+
     //temp store fd stream struct
-    typedef struct FDBUFFINF {
-        int count;//now store char count
-        char buff[MAXBUFF];//stream temp in
-    } FdBuff;
-    typedef struct FDDATA {
+    class Buffer {
+    public:
+        Buffer() : bCount(0), bCapacity(MAXBUFF) {
+            bBuff = new(std::nothrow) char[MAXBUFF];
+        }
+        Buffer(int Capacity) : bCount(0), bCapacity(Capacity) {
+            bBuff = new(std::nothrow) char[Capacity];
+        }
+        //judge current buffer is valid
+        bool isValid() { return bBuff!=nullptr; }
+        //get char's count in buffer
+        int count() { return bCount; }
+        //get capacity of buffer
+        int capacity() { return bCapacity; }
+        //append data to the tail of buffer array
+        int appendData(char* dataptr, int n) {
+            if(n <= 0 ) return 0;
+            int res = min(bCapacity-bCount,n);
+            memcpy(bBuff+bCount,dataptr,res);
+            bCount += res;
+            return res;
+        }
+    private:
+        int bCount;//now store char count
+        int bCapacity;//buffer can contain char's count
+        char *bBuff;//stream temp in
+    };
+
+    // typedef struct FDDATA {
+    //     int fd;
+    //     int type;
+    //     int len;
+    //     char buff[MAXBUFF];
+    // } FdData;
+
+    class Client {
+    public:
+        string ip;
+        int port;
         int fd;
-        int type;
-        int len;
-        char buff[MAXBUFF];
-    } FdData;
+
+        Client() : ip(""), fd(-1) {}
+        Client(string nip, int nport, int nfd) : ip(nip), port(nport), fd(nfd) {}
+        bool isValid() {
+            if(!buffer.isValid()) { return false; }
+        }
+    private:
+        Buffer buffer;
+    };
 
 
     #define HEADLEN 3
@@ -27,16 +71,18 @@ namespace NetWork {
         struct epoll_event* events;//wait to solve event
         int recv_len;//recv msg stream len
 
-        map<int,FdBuff> fd_buff; //fd temp buff;
+        map<int,shared_ptr<Client>> fd2client; 
 
-        queue<FdData> dataQ;//give out world data to use
+        //map<int,FdBuff> fd_buff; //fd temp buff;
+
+        //queue<FdData> dataQ;//give out world data to use
 
         
         //create socket and init
-        int InitSocket(int server_ip_port) {
+        int init(int server_ip_port = 8080) {
             memset(buff,0,sizeof(buff));
             memset(sbuff,0,sizeof(sbuff));
-            fd_buff.clear();
+            //fd_buff.clear();
             conn_fd = -1;
             listen_fd = -1;
             epoll_fd = -1;
@@ -98,20 +144,10 @@ namespace NetWork {
                 ERRP("events malloc");
                 close(listen_fd);
                 close(epoll_fd);
-                return 0;
+                return -1;
             }
-
+            return 0;
         }
-
-
-        int GetDatalen(char& a, char& b) {
-            int len = a-1;
-            len <<= 6;
-            len += b-1;
-            //TODO:len too long or other error judge, return -1
-            return len;
-        }
-
 
         //function to solve wait events
         int EventsSolve() {
@@ -126,8 +162,8 @@ namespace NetWork {
 
             for(int i = 0;i < n;i++) {
                 int fd = events[i].data.fd;
-                int event_fd = events[i].events;
-                if((!(event_fd & EPOLLIN))) {
+                int eventsinfo = events[i].events;
+                if((!(eventsinfo & EPOLLIN))) {
                     ERRNP("wait event i");
                     close(fd);
                     continue;
@@ -143,7 +179,29 @@ namespace NetWork {
                         ERRNP("new conn fd accept");
                         continue;
                     }
+                    struct sockaddr_in sa;
+                    socklen_t len;
+                    len = sizeof(sa);
 
+                    shared_ptr<Client> client(new Client());
+                    if(!client->isValid()) {
+                        ERRP("client new error");
+                        close(new_conn_fd);
+                        continue;
+                    }
+
+                    //get client information
+                    if(!getpeername(new_conn_fd, (struct sockaddr *)&sa, &len))
+                    {
+                        client->ip = inet_ntoa(sa.sin_addr);
+                        client->fd = new_conn_fd;
+                        client->port = ntohs(sa.sin_port);
+                    } else {
+                        ERRP("get client ip&port error");
+                        close(new_conn_fd);
+                        continue;
+                    }
+                    
                     //add new client fd to epoll
                     event.data.fd = new_conn_fd;
                     event.events = EPOLLIN;
@@ -153,19 +211,20 @@ namespace NetWork {
                         close(new_conn_fd);
                         continue;
                     }
+                    fd2client[new_conn_fd] = client;
                     cout << "new client connect" << endl;
                 }
                 else {
                     //if now fd no buff   give it a buff
-                    if(fd_buff.find(fd) == fd_buff.end()) {
-                    FdBuff temp;
-                    temp.count = 0;
-                    fd_buff[fd] = temp;
-                    }
+                    // if(fd_buff.find(fd) == fd_buff.end()) {
+                    // // FdBuff temp;
+                    // // temp.count = 0;
+                    // // fd_buff[fd] = temp;
+                    // }
                     //cout << "recv fd " << fd << endl;
 
                     //引用类型指向map中的对应fd
-                    FdBuff& t = fd_buff[fd];
+                    // FdBuff& t = fd_buff[fd];
 
                     if(t.count<HEADLEN) {
                         ret = recv_len = recv(fd,t.buff+t.count,HEADLEN-t.count,MSG_DONTWAIT);
@@ -206,13 +265,13 @@ namespace NetWork {
                         
                         //data is whole then push it to msg queue
                         if(t.count == HEADLEN+datalen) {
-                        FdData fddata;
-                        fddata.fd = fd;
-                        fddata.len = datalen;
-                        fddata.type = t.buff[0];
-                        memcpy(fddata.buff,t.buff+HEADLEN,datalen);
-                        dataQ.push(fddata);
-                        t.count = 0;
+                            FdData fddata;
+                            fddata.fd = fd;
+                            fddata.len = datalen;
+                            fddata.type = t.buff[0];
+                            memcpy(fddata.buff,t.buff+HEADLEN,datalen);
+                            dataQ.push(fddata);
+                            t.count = 0;
                         }
                     }
 
@@ -255,33 +314,6 @@ namespace NetWork {
             }
 
             return 1;
-        }
-
-        //broadcast buff to everyone in mp
-        int Broadcast(map<int,string>& mp, char* buff, int datatype, int datalen) {
-            buff[0] = datatype;
-            
-            buff[1] = (datalen>>6)+1;
-            buff[2] = datalen-(datalen>>6)+1;
-
-            int fd;
-            for(map<int,string>::iterator it = mp.begin();it != mp.end();it++) {
-                fd = it->first;
-                //cout << "broadcast fd " << fd << endl;
-                if(fd_buff.find(fd) == fd_buff.end()) {
-                    ERRP("broad cast");
-                    continue;
-                } 
-                ret = send(fd,buff,datalen+HEADLEN,0);
-                if(ret < 0) {
-                    ERRNP("broad cast send");
-                    close(fd);
-                    continue;
-                }
-                //for(int i = 0;i < datalen + HEADLEN;i++) {
-                //    cout << (int)buff[i] << " ";
-                //} cout << endl;
-            }
         }
 
         //close all fd

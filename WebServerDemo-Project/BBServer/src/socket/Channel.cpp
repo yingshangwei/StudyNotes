66932bd5d637f0f14781e741539c0c5e90852b12
yswdra <945394722@qq.com>
2021-06-03 20:29:34 +0800

    append some changes

--- WebServerDemo-Project/BBServer/src/socket/Channel.cpp
+++ WebServerDemo-Project/BBServer/src/socket/Channel.cpp
@@ -1,6 +1,7 @@
 #include "Channel.h"
 
 namespace BBS {
+
     Channel::Channel(int nBufferSize) {
         buffer = (char*)malloc(sizeof(char)*bufferSize);
         if(buffer == (char*)0) {
@@ -8,11 +9,32 @@ namespace BBS {
             return ;    
         }
         headPtr = 0;
-        tailPtr = -1;
+        tailPtr = 0;
+        dataCount = 0;
         bufferSize = nBufferSize;
     }
 
     bool Channel::isFull() {
-        return !bufferSize || tailPtr!=tailPtr;
+        return dataCount >= bufferSize;
+    }
+
+    int Channel::inData(const char* data, int n) {
+        int index = 0;
+        while(dataCount<bufferSize&&index<n) {
+            buffer[tailPtr++] = data[index++];
+            tailPtr %= bufferSize;
+            dataCount++;
+        }
+        return index;
+    }
+
+    int Channel::outData(const char* data, int maxData) {
+        int index = 0;
+        while(dataCount>0&&index<maxData) {
+            buffer[index++] = buffer[headPtr++];
+            headPtr %= bufferSize;
+            dataCount--;
+        }
+        return index;
     }
 }

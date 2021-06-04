6715dd52d9dd4d7ced47b72850574ef7895b669d
yswdra <945394722@qq.com>
2021-06-04 17:53:09 +0800

    day update : BBServer : add class Channel

--- WebServerDemo-Project/BBServer/src/socket/Channel.cpp
+++ WebServerDemo-Project/BBServer/src/socket/Channel.cpp
@@ -1,25 +1,46 @@
 #include "Channel.h"
 
 namespace BBS {
-
+    
     Channel::Channel(int nBufferSize) {
+        bufferSize = nBufferSize>0?nBufferSize:DEFAULT_CHANNEL_BUFFER_SIZE;
         buffer = (char*)malloc(sizeof(char)*bufferSize);
-        if(buffer == (char*)0) {
-            bufferSize = 0;
-            return ;    
+        if(buffer == nullptr) {
+            bufferSize = headPtr = tailPtr = dataCount = 0;
+            return ;
         }
         headPtr = 0;
         tailPtr = 0;
         dataCount = 0;
-        bufferSize = nBufferSize;
+    }
+    Channel::~Channel() {
+        if(buffer!=nullptr) free(buffer);
     }
 
     bool Channel::isFull() {
         return dataCount >= bufferSize;
     }
 
+    int Channel::reset(int n) {
+        std::lock_guard<std::mutex> lock(channelMutex);
+        if(n != bufferSize && n >= 0) {
+            if(buffer!=nullptr) free(buffer);
+            bufferSize = n;
+            buffer = (char*)malloc(sizeof(char)*bufferSize);
+            if(buffer == nullptr) {
+                bufferSize = headPtr = tailPtr = dataCount = 0;
+                return -1;
+            }
+        }
+        headPtr = 0;
+        tailPtr = 0;
+        dataCount = 0;
+    }
+
     int Channel::inData(const char* data, int n) {
+        std::lock_guard<std::mutex> lock(channelMutex);
         int index = 0;
+        //TODO::优化数据拷贝方案，考虑memory copy
         while(dataCount<bufferSize&&index<n) {
             buffer[tailPtr++] = data[index++];
             tailPtr %= bufferSize;
@@ -29,7 +50,10 @@ namespace BBS {
     }
 
     int Channel::outData(const char* data, int maxData) {
+        std::lock_guard<std::mutex> lock(channelMutex);
         int index = 0;
+        if(maxData < 0) maxData = bufferSize;
+        //TODO::优化数据拷贝方案，考虑memory copy
         while(dataCount>0&&index<maxData) {
             buffer[index++] = buffer[headPtr++];
             headPtr %= bufferSize;
--- WebServerDemo-Project/BBServer/src/socket/Channel.h
+++ WebServerDemo-Project/BBServer/src/socket/Channel.h
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <mutex>
-
-using namespace std;
+#include <vector>
+#include <thread>
 
 namespace BBS {
     const int DEFAULT_CHANNEL_BUFFER_SIZE = 1024;
@@ -10,15 +10,16 @@ namespace BBS {
     public :
         Channel(int nBufferSize = DEFAULT_CHANNEL_BUFFER_SIZE);
         bool isFull();
+        virtual int reset(int n = -1);
         virtual int inData(const char* data, int n);
         virtual int outData(const char* data, int maxData = -1);
-
+        virtual ~Channel();
     private:
         char *buffer;
         int bufferSize;
         int headPtr, tailPtr;
         int dataCount;
-                
+        std::mutex channelMutex;
     };
 
 }
